02ea08626c59a624617845419ecce213356ded28
ebc-2in2crc <y015h1n80@gmail.com>
2018-08-29 00:32:34 +0900

    Refactor: 関連する変数を1つの var にまとめる

--- gengo/gengo.go
+++ gengo/gengo.go
@@ -73,34 +73,33 @@ var values = []*Gengo{meiji, taisho, showa, heisei}
 
 var jst, _ = time.LoadLocation("Asia/Tokyo")
 
-var meiji = &Gengo{
-	"meiji",
-	"M",
-	"明治",
-	time.Date(1868, 1, 24, 23, 59, 59, 0, jst),
-	time.Date(1912, 7, 30, 0, 0, 0, 0, jst),
-}
-
-var taisho = &Gengo{
-	"taisho",
-	"T",
-	"大正",
-	time.Date(1912, 7, 29, 23, 59, 59, 0, jst),
-	time.Date(1926, 12, 25, 00, 00, 00, 0, jst),
-}
-
-var showa = &Gengo{
-	"showa",
-	"S",
-	"昭和",
-	time.Date(1926, 12, 24, 23, 59, 59, 0, jst),
-	time.Date(1989, 1, 8, 0, 0, 0, 0, jst),
-}
-
-var heisei = &Gengo{
-	"heisei",
-	"H",
-	"平成",
-	time.Date(1989, 1, 7, 23, 59, 59, 0, jst),
-	time.Date(9999, 12, 31, 23, 59, 59, 0, jst),
-}
+var (
+	meiji = &Gengo{
+		"meiji",
+		"M",
+		"明治",
+		time.Date(1868, 1, 24, 23, 59, 59, 0, jst),
+		time.Date(1912, 7, 30, 0, 0, 0, 0, jst),
+	}
+	taisho = &Gengo{
+		"taisho",
+		"T",
+		"大正",
+		time.Date(1912, 7, 29, 23, 59, 59, 0, jst),
+		time.Date(1926, 12, 25, 00, 00, 00, 0, jst),
+	}
+	showa = &Gengo{
+		"showa",
+		"S",
+		"昭和",
+		time.Date(1926, 12, 24, 23, 59, 59, 0, jst),
+		time.Date(1989, 1, 8, 0, 0, 0, 0, jst),
+	}
+	heisei = &Gengo{
+		"heisei",
+		"H",
+		"平成",
+		time.Date(1989, 1, 7, 23, 59, 59, 0, jst),
+		time.Date(9999, 12, 31, 23, 59, 59, 0, jst),
+	}
+)
